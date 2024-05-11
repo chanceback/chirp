f2ec3eb93f289beda80441e7cb7cbd4dfaeb4dad
Chance Back <chancedback@gmail.com>
2024-05-11 08:59:00 -0500

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={GeistSans.className}>
-      <Component {...pageProps} />
+      <ClerkProvider>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </main>
   );
 };
